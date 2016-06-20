ed207cf33f68319a6e0dd10f7b80d440c87c7a02
Povilas <povilas.pasausis@atosho.com>
2016-06-20 17:02:40 +0200

    Sets indentation to 2 spaces.

--- backend-api/models/Todo.js
+++ backend-api/models/Todo.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 
 var TodoSchema = new mongoose.Schema({
-	itemData: String,
-	completed: {
-		type: Boolean,
-		default: false
-	}
+  itemData: String,
+  completed: {
+    type: Boolean,
+    default: false
+  }
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
